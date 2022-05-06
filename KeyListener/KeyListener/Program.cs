67fc3725a14a5e122de51f503764fde40b2491a7
Andrew <stingeyandrewstown@live.com>
2022-05-06 16:15:17 -0500

    Additions

---
+++ KeyListener/KeyListener/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Timers;
+using static System.Console;
+
+namespace KeyListener
+{
+    public class KeyListener
+    {
+        static float Delay = 1000;
+        static System.Timers.Timer Timer = new System.Timers.Timer();
+        static ConsoleKey lastKeyPressed;
+        static TPiece piece = new TPiece();
+
+        static void Main()
+        {
+            KeyListener keyListener = new KeyListener();
+            keyListener.Start();
+        }
+
+        public void Start()
+        {
+            Clear();
+            piece.Reset();
+            WriteLine("Please press an arrow key...");
+
+            Timer.Elapsed += Tick;
+            Timer.Interval = Delay;
+            Timer.Start();
+
+            ConsoleKeyInfo info;
+            while ((info = ReadKey(true)) != null)
+                Input(info.Key);
+        }
+
+        static void Input(ConsoleKey key)
+        {
+            switch (key)
+            {
+                case ConsoleKey.LeftArrow:
+                    piece.Rotate(false);
+                    piece.DrawPiece();
+                    break;
+                case ConsoleKey.RightArrow:
+                    piece.Rotate(true);
+                    piece.DrawPiece();
+                    break;
+                case ConsoleKey.UpArrow:
+                case ConsoleKey.DownArrow:
+                case ConsoleKey.Spacebar:
+                    Tick(key, null);
+                    break;
+                case ConsoleKey.Escape:
+                    Environment.Exit(Environment.ExitCode);
+                    break;
+            }
+        }
+
+        static void Tick(object sender, ElapsedEventArgs e)
+        {
+            if (sender is ConsoleKey)
+            {
+                ConsoleKey pressed = (ConsoleKey)sender;
+                if (pressed != lastKeyPressed)
+                {
+                    lastKeyPressed = pressed;
+                    ConsoleWriteKey();
+                }
+            }
+
+            Timer.Interval = Delay;
+            Timer.Start();
+        }
+
+        static void ConsoleWriteKey()
+        {
+            SetCursorPosition(0, 0);
+            Console.Write(new string(' ', Console.WindowWidth));
+            SetCursorPosition(0, 0);
+            Write(lastKeyPressed);
+            SetCursorPosition(1, 1);
+            Console.Write("┌──────┐");
+            SetCursorPosition(1, 2);
+            Console.Write("│      │");
+            SetCursorPosition(1, 3);
+            Console.Write("│      │");
+            SetCursorPosition(1, 4);
+            Console.Write("│      │");
+            SetCursorPosition(1, 5);
+            Console.Write("└──────┘");
+        }
+    }
+}
---
+++ KeyListener/KeyListener/TPiece.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using static System.Console;
+
+namespace KeyListener
+{
+    public class TPiece
+    {
+        char[] Body = new char[] { ' ', '▉', ' ', '▉', '▉', '▉', ' ', ' ', ' ' };
+        const int Width = 3;
+        const int xStart = 2;
+        const int yStart = 2;
+
+        public void Reset()
+        {
+            Body = new char[] { ' ', '▉', ' ', '▉', '▉', '▉', ' ', ' ', ' ' };
+        }
+
+        public void Rotate(bool IsRight)
+        {
+            char[] Temp = new char[(Width * Width)];
+            for (int i = 0; i < (Width * Width); i++)
+            {
+                Temp[i] = IsRight ? Body[i + (((Width * Width) - Width) + (((-Width + 1) * (i / Width)) + ((-Width - 1) * (i % Width))))] : Body[i + ((-1 + Width) + (((-Width - 1) * (i / Width)) + ((Width - 1) * (i % Width))))];
+            }
+            for (int i = 0; i < (Width * Width); i++)
+            {
+                Body[i] = Temp[i];
+            }
+        }
+
+        public void DrawPiece()
+        {
+            for (int y = 0; y < Width; y++)
+            {
+                SetCursorPosition(xStart, yStart + y);
+                for (int x = 0; x < Width; x++)
+                {
+                    Console.Write(Body[x+(y*Width)]+""+Body[x + (y * Width)]);
+                }
+            }
+        }
+    }
+}
